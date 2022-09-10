21b28a45d3573fa7d9ac692f84cae98ed6072f8d
NotAShelf <itsashelf@gmail.com>
2022-09-10 21:46:59 +0300

    reorganize zsh

---
+++ .config/zsh/aliases.zsh
@@ -0,0 +1,17 @@
+# flatpak aliases
+alias discord='flatpak run com.discordapp.Discord'
+alias spotify='flatpak run com.spotify.Client'
+alias flameshot='flatpak run org.flameshot.Flameshot'
+alias zoom='flatpak run us.zoom.Zoom'
+alias flatseal='flatpak run com.github.tchx84.Flatseal'
+
+# my own aliases
+alias grep='rg'  # ripgrep
+alias  cat='bat' # bat
+alias   ls='lsd' # lsd
+alias maid='npm' # node package maid lol
+alias find='fd'  # fd
+
+# zsh specific aliases
+alias zshconfig="nano $XDG_CONFIG_HOME/zsh/.zshrc"
+alias ohmyzsh="nano $XDG_DATA_HOME/oh-my-zsh/.oh-my-zsh"
