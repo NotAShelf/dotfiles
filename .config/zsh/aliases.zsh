f3b1ddeb11250ce8a1861e3bb319c34d5feb4a7b
NotAShelf <itsashelf@gmail.com>
2022-09-19 20:46:50 +0300

    update discord alias

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,5 +1,5 @@
 # flatpak aliases
-alias discord='flatpak run com.discordapp.Discord'
+#alias discord='flatpak run com.discordapp.Discord'
 alias spotify='flatpak run com.spotify.Client'
 alias flameshot='flatpak run org.flameshot.Flameshot'
 alias zoom='flatpak run us.zoom.Zoom'
@@ -11,6 +11,9 @@ alias  cat='bat' # bat
 alias   ls='lsd' # lsd
 alias maid='npm' # node package maid lol
 alias find='fd'  # fd
+
+# discord with funny gpu accel
+alias discord='discord-canary --ignore-gpu-blocklist --disable-features=UseOzonePlatform --enable-features=VaapiVideoDecoder --use-gl=desktop --enable-gpu-rasterization --enable-zero-copy'
 
 # zsh specific aliases
 alias zshconfig='nano "$XDG_CONFIG_HOME"/zsh/.zshrc'
