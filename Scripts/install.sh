b47b0c7c60c8ca58e1165c6ab98457c2b3e0269c
NotAShelf <itsashelf@gmail.com>
2022-10-16 16:51:41 +0300

    fix syntax issues in the script

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -2,9 +2,18 @@
 
 
 # Get the name of the currently executed script
-SCRIPTNAME=$(basename $0)
+SCRIPTNAME=$(basename "$0")
 # Get the directory of the currently executed script
 SCRIPTDIR=$(pwd)
+# Find an AUR helper to install packages
+if [ "$(command -v yay)" ]; then
+  HELPER="yay"
+elif [ "$(command -v paru)" ]; then
+  HELPER="paru"
+else
+  echo ":: No AUR helper found. Exiting..."
+  exit 0
+fi
 
 # If the script is ran without an argument, display the output message
 if [ $# -eq 0 ]; then
@@ -16,9 +25,9 @@ if [ $# -eq 0 ]; then
   exit 1
 fi
 
-if [ $1 -ne "--install"] || [ $1 -ne "--backup" ] || [ $1 -ne "--apply" ]; then
+if [ "$1" != "--install" ] && [ "$1" != "--backup" ] && [ "$1" != "--apply" ]; then
   echo "Invalid flag specified. Try --help to see available options"
-  if [ $1 == "--help "]
+  if [ "$1" == "--help" ]; then
 	  echo -en "Usage:\n
 	              $SCRIPTNAME --install - Installs packages from the latest packages-<date>.list file\n
 	              $SCRIPTNAME --backup  - Generates a list of installed packages and saves it to a file\n
@@ -36,16 +45,12 @@ fi
 
 # if an error code is emitted, ask if the user wants to continue
 if [ "$?" -ne 0 ]; then
-  read -p "An error occured. Do you still want to continue? [y/n]" answer
+  read -rp "An error occured. Do you still want to continue? [y/n]" answer
   case $answer in
     y|Y) echo "Continuing...";;
     n|N) echo "Exiting..."; exit 1;;
     *) echo "Invalid input. Exiting..."; exit 1;;
   esac
-  # if [ "$answer" == "n" ] || [ "$answer" == "N" ]; then
-  #   echo "Aborting..."
-  #   exit 1
-  # fi
 fi
 
 # If script is run with "--install" as the first argument, install the packages specified in the latest updated
@@ -56,32 +61,25 @@ if [ "$1" == "--install" ]; then
   packages="$SCRIPTDIR"/"$LATEST"
   echo "Installing packages from '${packages}'"
 
-  if ! pacman -Qq $(cat $SCRIPTDIR/packages-*.list) >/dev/null 2>&1; then
-   if [ "$(command -v yay)" ]; then
-      HELPER="yay"
-   elif [ "$(command -v paru)" ]; then
-      HELPER="paru"
-   else
-      echo ":: No AUR helper found. Exiting..."
-      exit 0
-   fi
-
-  for package in $packages
-  do
-    echo "Installing '${package}'"
-    sudo ${HELPER} -S '${package}' --noconfirm
-  done
+  if ! pacman -Qq "$(cat "$SCRIPTDIR"/packages-*.list)" >/dev/null 2>&1; then
+    echo "Installing packages..."
+    for package in $packages
+    do
+      echo "Installing '${package}'"
+      sudo ${HELPER} -S '${package}' --noconfirm
+    done
+  fi
 fi
 
 # If the script is run with "--apply" as the first argument, list all files in .config and override ~/.config with them
 # in $SCRIPTDIR
 if [ "$1" == "--apply" ]; then
-  FILES=$(tree -if ${SCRIPTDIR}/.config)
+  FILES=$(tree -if "${SCRIPTDIR}"/.config)
 
   for file in $FILES
   do
     echo "Copying '${file}' to ~/.config"
-    diff -q ${file} ~/.config/${file}
+    diff -q "${file}" ~/.config/"${file}"
     cp -rv "${file}" ~/.config
   done
 
@@ -90,8 +88,10 @@ fi
 # If script is run with "--backup" as the first argument, generate a list of installed packages and save it to
 # packages-DATE.list
 if [ "$1" == "--backup" ]; then
-  echo "Backing up system packages"
-  CURBACKUP=packages-"$(date +%y%m%d%H%M)".list
-  pacman -Qe | awk '{ print $1 }' > "${CURBACKUP}"
+  echo "Generating list of installed packages..."
+  # Set CURBACKUP as packages-YYDDMMHHMM.list
+  CURBACKUP="packages-'$(date +%y%d%m%H%M)'.list"
+  # Generate a list of installed packages and save it to $CURBACKUP
+  pacman -Qqe > "${SCRIPTDIR}"/"${CURBACKUP}"
   echo "Systems packages backed up at '${CURBACKUP}'"
 fi
