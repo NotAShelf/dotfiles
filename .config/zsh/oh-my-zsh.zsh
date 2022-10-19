43bbb6a567fb168c0e32c58fba3162eda4bc017d
NotAShelf <itsashelf@gmail.com>
2022-10-20 00:10:57 +0300

    spicetify + swaync chapter 1251

--- .config/zsh/oh-my-zsh.zsh
+++ .config/zsh/oh-my-zsh.zsh
@@ -1,5 +1,5 @@
 # If you come from bash you might have to change your $PATH.
-# export PATH=$HOME/bin:/usr/local/bin:$PATH
+export PATH=$PATH:/home/notashelf/.spicetify
 
 # Set name of the theme to load --- if set to "random", it will
 # load a random theme each time oh-my-zsh is loaded, in which case,
